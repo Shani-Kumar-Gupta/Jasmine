ba78967893f66a3eb95c9868cc5affe22fc15ebe
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-03-07 00:09:03 +0530

    Jasmine standalone setup done | npm setup will do

---
+++ projects/jasmineSetup/main.js
@@ -0,0 +1,43 @@
+function calc(event) {
+  console.log(event.target.value);
+  const inputValue = event.target.value;
+  const expression = /\+|\*|\-|\//;
+  const values = inputValue.split(expression);
+  const numberA = +values[0];
+  const numberB = +values[1];
+  const operation = inputValue.match(expression);
+  if (isNaN(numberA) || isNaN(numberB) || operation == null) {
+    updateResult('Operation not recognized');
+    return;
+  }
+  const operator = operation[0];
+
+  const cal = new Calculator();
+  cal.add(numberA);
+  let result;
+  switch (operator) {
+    case '+':
+      result = cal.add(numberB);
+      break;
+    case '-':
+      result = cal.subtract(numberB);
+      break;
+    case '*':
+      result = cal.multiply(numberB);
+      break;
+    case '/':
+      result = cal.divide(numberB);
+      break;
+  }
+  // console.log("My Result: ", result, operator, '+' === operator, numberA, numberB);
+  // document.getElementById("myResult").innerText = result;
+  updateResult(result);
+}
+
+function updateResult(result) {
+  let ele = document.getElementById("myResult");
+  if (ele) {
+    ele.innerText = result;
+  }
+}
+document.getElementById('inputValue').addEventListener('change', calc);
