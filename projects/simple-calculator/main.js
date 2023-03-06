4ba7e0266de15b2d541e5be8691009a6a6154337
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-03-06 23:41:18 +0530

    Jasmine: Calculator logic implemented

--- projects/simple-calculator/main.js
+++ projects/simple-calculator/main.js
@@ -4,13 +4,36 @@ function calc(event) {
   const expression = /\+|\*|\-|\//;
   const values = inputValue.split(expression);
   const numberA = +values[0];
-  const numberB = +values[2];
+  const numberB = +values[1];
   const operation = inputValue.match(expression);
   const operator = operation[0];
 
   const cal = new Calculator();
-  cal.add(4);
-  cal.add(5);
-  console.log("values::", numberA, numberB, operator, operation, cal.total);
+  cal.add(numberA);
+  let result;
+  switch (operator) {
+    case '+':
+      result = cal.add(numberB);
+      break;
+    case '-':
+      result = cal.subtract(numberB);
+      break;
+    case '*':
+      result = cal.multiply(numberB);
+      break;
+    case '/':
+      result = cal.divide(numberB);
+      break;
+  }
+  // console.log("My Result: ", result, operator, '+' === operator, numberA, numberB);
+  // document.getElementById("myResult").innerText = result;
+  updateResult(result);
+}
+
+function updateResult(result) {
+  let ele = document.getElementById("myResult");
+  if (ele) {
+    ele.innerText = result;
+  }
 }
 document.getElementById('inputValue').addEventListener('change', calc);
