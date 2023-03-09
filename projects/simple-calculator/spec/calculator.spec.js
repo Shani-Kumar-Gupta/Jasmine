c6c5094437400ad5f37ad7a778ce042f61e51d68
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-03-09 23:33:40 +0530

    🚀 Jasmine -> spec file -> test suites -> specs or test cases -> Expectation

--- projects/simple-calculator/spec/calculator.spec.js
+++ projects/simple-calculator/spec/calculator.spec.js
@@ -1,5 +1,26 @@
 // Unit tests using suites
 // Test or spec suites -> Group of spec or tests
+// We can create multiple test suites inside one file
+
+// What is Spec - A Spec is a group of expectation that test the state of the application or code
 
 // How to write your first test suite??
+describe('Description || Title : calculator.js', function () {
+  // TODO: write tests or specs
+  // Test Cases or Specs Cases - 
+  it('Calculator.js : Should add the two numbers', function () {
+    // TODO: Expectations
+  });
 
+  it('Claculator.js : Should subtract the two numbers', function () {
+    // TODO: Expectations
+  });
+
+  it('Calculator.js : Should multiply the two numbers', function () {
+    // TODO: Expectations
+  });
+
+  it('Calculator.js : Should divide the two numbers', function () {
+    // TODO: Expectations
+  });
+}); // It's a method to create the suite - describe(arg1 -> 'Title or description', arg2 -> Anonymous function);
